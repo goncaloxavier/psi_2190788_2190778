8bbb1157b30dd18ae48c01f50d4ade475d98d3c7
Gonçalo Venâncio Xavier <2190788@my.ipleiria.pt>
2020-10-19 20:38:25 +0100

    AM (Android)
    Layouts

---
+++ AM/app/src/main/java/amsi/dei/estg/ipleiria/am/MainActivity.java
@@ -0,0 +1,14 @@
+package amsi.dei.estg.ipleiria.am;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
