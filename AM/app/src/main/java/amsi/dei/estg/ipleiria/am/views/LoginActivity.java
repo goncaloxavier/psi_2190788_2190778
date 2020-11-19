95be0083fa8f5c6c56612b08a9e370031138c7ec
Gonçalo Venâncio Xavier <2190788@my.ipleiria.pt>
2020-11-19 21:34:18 +0000

    Exception when changing views

--- AM/app/src/main/java/amsi/dei/estg/ipleiria/am/views/LoginActivity.java
+++ AM/app/src/main/java/amsi/dei/estg/ipleiria/am/views/LoginActivity.java
@@ -2,15 +2,34 @@ package amsi.dei.estg.ipleiria.am.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import amsi.dei.estg.ipleiria.am.R;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private EditText edtUsername, edtPassword;
+    private Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        edtUsername = findViewById(R.id.edtUsername);
+        edtPassword = findViewById(R.id.edtPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+    }
+
+    public void onClickLogin(View view) {
+        String username = edtUsername.getText().toString();
+        String password = edtPassword.getText().toString();
+
+        Intent intent = new Intent(this, MenuTecnicoActivity.class);
+        intent.putExtra(MenuTecnicoActivity.USERNAME, username);
+        startActivity(intent);
+        System.out.println("--> Username: " + username + "  Password: " + password);
     }
 }
--- AM/app/src/main/java/amsi/dei/estg/ipleiria/am/views/MenuTecnicoActivity.java
+++ AM/app/src/main/java/amsi/dei/estg/ipleiria/am/views/MenuTecnicoActivity.java
@@ -3,14 +3,27 @@ package amsi.dei.estg.ipleiria.am.views;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import amsi.dei.estg.ipleiria.am.R;
 
 public class MenuTecnicoActivity extends AppCompatActivity {
 
+    public static final String USERNAME = "USERNAME";
+    private String username = "";
+    private TextView tvUsername;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_tecnico);
+
+        tvUsername = findViewById(R.id.tvUsername);
+        definirUsername();
+    }
+
+    private void definirUsername(){
+        username = getIntent().getStringExtra(USERNAME);
+        tvUsername.setText(username);
     }
 }
